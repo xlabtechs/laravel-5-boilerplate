4efe704a404aac6773cdf3533e8791dd272344af
Ahmad-Alhourani <39119282+Ahmad-Alhourani@users.noreply.github.com>
2018-08-15 22:15:12 +0300

    update backend

--- routes/breadcrumbs/backend/backend.php
+++ routes/breadcrumbs/backend/backend.php
@@ -4,5 +4,7 @@ Breadcrumbs::register('admin.dashboard', function ($breadcrumbs) {
     $breadcrumbs->push(__('strings.backend.dashboard.title'), route('admin.dashboard'));
 });
 
+//*****Do Not Delete Me
+
 require __DIR__.'/auth.php';
 require __DIR__.'/log-viewer.php';
